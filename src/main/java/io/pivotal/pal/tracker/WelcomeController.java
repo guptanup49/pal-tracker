2df3c4e25df6dd028dea4b1c727f35fab6a2135a
Anup_Gupta <agupta8@humana.com>
2019-05-06 15:00:18 -0600

    second commit

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,4 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+    //@Value("${welcome.message}")
+    private String msg;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return this.msg;
+    }
+    public WelcomeController (@Value("${welcome.message}") String message) {
+        this.msg = message;
+
+    }
 }
