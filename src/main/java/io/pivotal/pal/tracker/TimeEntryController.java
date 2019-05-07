5431fc60f8d1bee735b1c11e594677789449303a
Anup_Gupta <agupta8@humana.com>
2019-05-07 12:58:38 -0600

    working still

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,7 +1,7 @@
 package io.pivotal.pal.tracker;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -11,22 +11,27 @@ public class TimeEntryController {
 
     }
 
+    @PostMapping
     public ResponseEntity create(TimeEntry timeEntryToCreate) {
         return null;
     }
 
+    @GetMapping
     public ResponseEntity<TimeEntry> read(long timeEntryId) {
         return null;
     }
 
+    @GetMapping
     public ResponseEntity<List<TimeEntry>> list() {
         return null;
     }
 
+    @PutMapping
     public ResponseEntity update(long timeEntryId, TimeEntry expected) {
         return null;
     }
 
+    @DeleteMapping
     public ResponseEntity<TimeEntry> delete(long timeEntryId) {
         return null;
     }
